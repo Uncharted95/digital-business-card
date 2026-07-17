be9b8df1d031124c527bb80dc1fa82389b5e3ae7
Alexander A <41549546+Uncharted95@users.noreply.github.com>
2026-07-17 16:04:56 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,17 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquareTwitter } from "@fortawesome/free-solid-svg-icons";
+import { faSquareFacebook } from "@fortawesome/free-solid-svg-icons";
+import { faSquareInstagram } from "@fortawesome/free-solid-svg-icons";
+import { faSquareGithub } from "@fortawesome/free-solid-svg-icons";
+
+
+export default function Icons(){
+    return(
+        <>
+        <FontAwesomeIcon icon={faSquareGithub} className="icon-Github"/>
+        <FontAwesomeIcon icon={faSquareInstagram} className="icon-Instagram"/>
+        <FontAwesomeIcon icon={faSquareFacebook} className="icon-Facebook"/>
+        <FontAwesomeIcon icon={faSquareTwitter} className="icon-Twitter"/>
+        </>
+    )
+}

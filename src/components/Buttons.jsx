956a5333926a3a41970d60c2f4d99064f33eb1ae
Alexander A <41549546+Uncharted95@users.noreply.github.com>
2026-07-17 15:41:53 +0200

    Set up of Components
    Started building up the components for later usage

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,19 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faAt } from "@fortawesome/free-solid-svg-icons";
+
+export default function Buttons() {
+  return (
+    <>
+      <button className="btn-email">
+        <FontAwesomeIcon icon={faAt} />
+        Email
+      </button>
+
+      <button className="btn-linkedin">
+        <FontAwesomeIcon icon={faLinkedin} />
+        LinkedIn
+      </button>
+    </>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function header(){
+    return(
+        <>
+        <header>
+            <img className="image" src="/assets/profile.jpg" alt="head shot for company"/>
+        </header>
+        <h1>Alexander Aldilemi</h1>
+        <h2>UX Engineer</h2>
+        <h3>AlexAldilemi.website</h3>
+        </>
+
+    )
+}
